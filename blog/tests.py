eb9d78ca8ea4af37db95ab1f56fc3621d1ab02f9
guidomperosino <guidomperosino@gmail.com>
2020-03-28 23:13:44 -0300

    actualizacioin

--- blog/tests.py
+++ blog/tests.py
@@ -1,3 +1,13 @@
 from django.test import TestCase
 
-# Create your tests here.
+# Create your tests here
+# Ejecutar la vista Post New
+# Asignar Valores de Post prueba (cuerpo titulo user)
+# Continuar proceso, crear breakpoint, refresh from DB y chequear si Published Date quedo grabado
+# Borrar el post.
+# quitar el Breakpoint"
+post.title='testing'
+post.text='posting'
+
+request	post_new
+
